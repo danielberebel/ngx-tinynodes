1f7cf0ed029cb70550bfcc2d74ff47e3bc0c51d6
Dani <daniel@berebel.io>
2021-04-13 17:36:20 +0200

    Update dependencies

--- libs/ngx-tinynodes-core/src/lib/ngx-tinynodes-core-material.module.ts
+++ libs/ngx-tinynodes-core/src/lib/ngx-tinynodes-core-material.module.ts
@@ -8,6 +8,6 @@ import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [CommonModule, MatGridListModule, MatCardModule, MatExpansionModule, MatListModule, MatButtonModule],
-  exports: [CommonModule, MatGridListModule, MatCardModule, MatExpansionModule, MatListModule, MatButtonModule]
+  exports: [CommonModule, MatGridListModule, MatCardModule, MatExpansionModule, MatListModule, MatButtonModule],
 })
 export class NgxTinynodesCoreMaterialModule {}
